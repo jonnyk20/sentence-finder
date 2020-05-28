79e0a19747b70fcfd648ce05396eede6f96e4daf
Jonny Kalambay <jonnyk20@gmail.com>
2020-05-27 21:55:44 -0700

    Self join works

--- rails-backend/app/models/sentence.rb
+++ rails-backend/app/models/sentence.rb
@@ -1,26 +1,32 @@
 class Sentence < ApplicationRecord
   self.table_name = "Sentences"
 
+  has_many :sentence_translations, foreign_key: 'sentenceId', primary_key: 'tatoebaId'
+  has_many :translations, :through => :sentence_translations, foreign_key: 'sentenceId', primary_key: 'tatoebaId'
+
+  has_many :inverse_sentence_translations, class_name: 'SentenceTranslation', foreign_key: 'translationId', primary_key: 'sentenceId'
+  has_many :inverse_translations, :through => :inverse_sentence_translations, :source => :sentence, foreign_key: 'translationId', primary_key: 'tatoebaId'
+
   # belongs :translation, class_name: 'Sentence', foreign_key: 'tatoebaId'
 
   # belongs_to :parent, class_name 'Member'
   # this one worked
-  has_many :sentence_translations, class_name: 'SentenceTranslation', foreign_key: 'sentenceId', primary_key: 'tatoebaId'
+  # has_many :sentence_translations, class_name: 'SentenceTranslation', foreign_key: 'sentenceId', primary_key: 'tatoebaId'
 
 
-  def get_translations(language)
-    sentence_translations = self.sentence_translations
+  # def get_translations(language)
+  #   sentence_translations = self.sentence_translations
 
-    translations = sentence_translations
-      .map { |s_t| s_t.translation }
+  #   translations = sentence_translations
+  #     .map { |s_t| s_t.translation }
 
-    # filtered_translations = translations.select {|s| s.language == language }
-    print('translations?')
-    print(translations.count)
+  #   # filtered_translations = translations.select {|s| s.language == language }
+  #   print('translations?')
+  #   print(translations.count)
 
-    {
-      sentence: self,
-      translations: []# filtered_translations
-    }
-  end
+  #   {
+  #     sentence: self,
+  #     translations: []# filtered_translations
+  #   }
+  # end
 end
--- rails-backend/app/models/sentence_translation.rb
+++ rails-backend/app/models/sentence_translation.rb
@@ -1,6 +1,9 @@
 class SentenceTranslation < ApplicationRecord
   self.table_name = "SentenceTranslations"
 
-  # this one 
-  has_one :translation, class_name: 'Sentence', foreign_key: 'tatoebaId', primary_key: 'translationId'
+  belongs_to :sentence
+  belongs_to :translation, class_name: 'Sentence', foreign_key: 'translationId', primary_key: 'tatoebaId'
+
+  # # this one 
+  # has_one :translation, class_name: 'Sentence', foreign_key: 'tatoebaId', primary_key: 'translationId'
 end
